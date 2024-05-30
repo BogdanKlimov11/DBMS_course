148bc1c382ab22b3ec6066a47abe335268f53cb7
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-05-30 14:25:26 +0300

    Create Inserting_data.sql

---
+++ Examples/Inserting_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Employees (FirstName, LastName, BirthDate, HireDate, Salary)
+VALUES ('John', 'Doe', '1980-05-15', '2020-03-01', 60000.00);
+
+INSERT INTO Employees (FirstName, LastName, BirthDate, HireDate, Salary)
+VALUES ('Jane', 'Smith', '1990-07-20', '2021-06-15', 65000.00);
